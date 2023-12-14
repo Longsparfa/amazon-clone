3c794e4ba15e7701e807a9eb287dece55a7db670
Longs Parfa <longsparfa@gmail.com>
2023-12-14 09:59:05 -0800

    update search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,6 +5,7 @@ import { callAPI } from "../utils/CallApi";
 const Search = () => {
   const [suggestions, setSuggestions] = useState(null);
   const [searchTerm, setSearchTerm] = useState("");
+  const [category, setCategory] = useState("All");
 
   const getSuggestion = () => {
     callAPI(`data/suggestions.json`).then((suggestionResults) => {
@@ -19,7 +20,7 @@ const Search = () => {
   return (
     <div className="w-[100%] ">
       <div className="flex items-center h-10 bg-amazonclone-yellow rounded ">
-        <select className="p-2 bg-gray-300 text-black border text-xs xl:text-sm">
+        <select onChange={(e) => setCategory(e.target.value)} className="p-2 bg-gray-300 text-black border text-xs xl:text-sm">
           <option>All</option>
           <option>Deals</option>
           <option>Amazon</option>
@@ -34,7 +35,7 @@ const Search = () => {
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <button className="w-[48px] ">
+        <button  className="w-[48px] ">
           <MagnifyingGlassIcon className="h-[27px] m-auto stroke-slate-900" />
         </button>
       </div>
@@ -59,5 +60,20 @@ const Search = () => {
     </div>
   );
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default Search;
